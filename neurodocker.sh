e61d97fcca5a772fa18fc7a7e0cb08951fe6147f
Denis Chaimow <dchaimow@users.noreply.github.com>
2021-02-05 09:03:39 +0100

    changed to neurodebian

--- neurodocker.sh
+++ neurodocker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 docker run --rm repronim/neurodocker:0.7.0 generate docker \
-           --base debian:stretch --pkg-manager apt \
-           --fsl version=6.0.3 \
-           --miniconda create_env=neuro \
-                       conda_install="python=3.8 traits" \
-                       pip_install="nipype" > Dockerfile
+       --base neurodebian:stretch-non-free \
+       --pkg-manager apt \
+       --fsl version=6.0.3 \
+       --miniconda create_env=neuro \
+       conda_install="python=3.8 traits" \
+       pip_install="nipype" > Dockerfile
